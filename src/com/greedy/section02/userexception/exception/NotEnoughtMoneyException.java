e56be2d47d4258e71045d000061bc5d39db6edd0
tjoeun <tjoeun@DESKTOP-PJLQ947>
2023-02-02 09:31:54 +0900

    ch13

---
+++ src/com/greedy/section02/userexception/exception/NotEnoughtMoneyException.java
@@ -0,0 +1,10 @@
+package com.greedy.section02.userexception.exception;
+
+public class NotEnoughtMoneyException extends Exception {
+
+	public NotEnoughtMoneyException() {}
+	
+	public NotEnoughtMoneyException(String message) {
+		super(message);
+	}
+}
